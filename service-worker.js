5aa0a5e80cb5eed20b74755a63c0817a60344eec
initiumdev <dev+github@theinitium.com>
2020-07-24 21:48:08 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1766a2911016073dcf5f3f38e022a5f8.js"
+  "./precache-manifest.86a193a9bec3f42de1c08ac45e368af8.js"
 );
 
 self.addEventListener('message', (event) => {
